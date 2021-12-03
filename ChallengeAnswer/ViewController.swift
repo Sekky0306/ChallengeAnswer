84f05b98c5beb58765fbd65dd7186fa435a0213f
YukiSekido <step1336@gmail.com>
2021-12-03 17:07:12 +0900

    aaaaa

--- ChallengeAnswer/ViewController.swift
+++ ChallengeAnswer/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        self.changeColor()
+    }
+    @IBAction func hiku(){
+           number = number - 1
+           label.text = String(number)
+           self.changeColor()
+    }
+    @IBAction func clear(){
+               number = 0
+               label.text = String(number)
+        self.changeColor()
+           }
+    func changeColor(){
+        if number >= 10 {
+            label.textColor = UIColor.red
+        }else{
+            label.textColor = UIColor.black
+        }
+    }
 
 }
 
